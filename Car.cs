f12e71d132ab55585ca81f6b3d653db07a96521d
3472592 <3472592@gmail.com>
2024-06-27 05:09:04 -0400

    Add files via upload
    Pracrice.

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3WinFormsApp2
+{
+    class Car
+    {
+        public string Name;
+        public string Design;
+        public int HorsePower;
+        public int MaxRPM;
+        public int MaxSpeed;
+        public decimal EngineLitters;
+        public string color;
+        public string RimsColor;
+        public string Equipment;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Ch3WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
